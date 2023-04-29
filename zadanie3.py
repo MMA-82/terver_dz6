8a2a1d3dcad720eb0077d6bb42026b023361e372
Marat <brutto061@gmail.com>
2023-04-29 15:17:16 +0700

    +code

--- zadanie3.py
+++ zadanie3.py
@@ -4,13 +4,13 @@ import scipy.stats as stats
 d = np.array([175, 167, 154, 174, 178, 148, 160, 167, 169, 170])
 m = np.array([178, 165, 165, 173, 168, 155, 160, 164, 178, 175])
 
-delta = np.mean(d) - np.mean(m)
-
+delta = np.mean(m) - np.mean(d)
+print(np.mean(m), np.mean(d), delta)
 D = (np.var(d, ddof=1) + np.var(m, ddof=1))/ 2
-
+print(D)
 S = np.sqrt(D/ len(d) + D/ len(m))
-
+print(S)
 k = 2*(len(d) - 1)
 t = stats.t.ppf(0.975, k)
-
+print(t)
 print(f'Доверительный интервал для разности среднего роста родителей и детей: [{delta - t*S}; {delta + t*S}]')
